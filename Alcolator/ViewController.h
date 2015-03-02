ced16e13e8beacf5a7e5886a9749c7bdab18516a
RH Blanchfield <arttechlife@gmail.com>
2015-03-02 06:07:55 -0800

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by RH Blanchfield on 3/2/15.
+//  Copyright (c) 2015 bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
